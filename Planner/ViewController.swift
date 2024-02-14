4155697289836f38867b41dd4a5da2e09a8a3104
Minchelin42 <mje0211@naver.com>
2024-02-14 23:50:31 +0900

    Initial Commit

---
+++ Planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planner
+//
+//  Created by 민지은 on 2024/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
