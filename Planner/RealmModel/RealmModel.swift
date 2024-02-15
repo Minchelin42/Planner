43ead4f6ec3e7efd8048553e66b5783945bd859d
Minchelin42 <mje0211@naver.com>
2024-02-15 20:57:06 +0900

    FEAT: RealmModel 적용

---
+++ Planner/RealmModel/RealmModel.swift
@@ -0,0 +1,33 @@
+//
+//  RealmModel.swift
+//  Planner
+//
+//  Created by 민지은 on 2024/02/15.
+//
+
+import Foundation
+import RealmSwift
+//제목, 메모, 마감일, 태그, 우선순위
+
+class PlannerTable: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var memo: String?
+    @Persisted var date: String
+    @Persisted var tag: String
+    @Persisted var priority: String
+    
+    convenience init(
+        title: String,
+        memo: String? = nil,
+        date: String,
+        tag: String,
+        priority: String) {
+        self.init()
+        self.title = title
+        self.memo = memo
+        self.date = date
+        self.tag = tag
+        self.priority = priority
+    }
+}
